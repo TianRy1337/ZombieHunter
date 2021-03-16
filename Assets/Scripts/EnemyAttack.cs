ce8df215f1b12579ddb7c6b479d42c4fd845cc65
TianRy1337 <Ryan55014TianZhang@gmail.com>
2021-03-16 18:58:29 +0800

    重新上傳

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    PlayerHealth target;
+    [SerializeField] float damage = 40f;
+
+    private void Start()
+    {
+        target = FindObjectOfType<PlayerHealth>();
+    }
+    public void AttackHitEvent()
+    {
+        if(target == null)return;
+        target.TakeDamage(damage);
+    }
+}
